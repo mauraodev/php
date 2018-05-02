4233c31efd61898b00c277deeaf1a3d8d70bc2b9
Mauro Nunes <mauraoemau@hotmail.com>
2018-05-02 09:37:45 -0300

    Identação de código fonte

--- acentos/index.php
+++ acentos/index.php
@@ -1,38 +1,36 @@
 <?php
 
-   function url_amigavel($string){
+function url_amigavel($string)
+{
+  $string    = str_replace(array("á","à","â","ã","ä","ª") ,"a",$string);
+  $string    = str_replace(array("Á","À","Â","Ã","Ä") ,"a",$string);
 
-      $string    = str_replace(array("á","à","â","ã","ä","ª") ,"a",$string);
-      $string    = str_replace(array("Á","À","Â","Ã","Ä") ,"a",$string);
+  $string    = str_replace(array("é","è","ê","ë") ,"e",$string);
+  $string    = str_replace(array("É","È","Ê","Ë") ,"e",$string);
 
-      $string    = str_replace(array("é","è","ê","ë") ,"e",$string);
-      $string    = str_replace(array("É","È","Ê","Ë") ,"e",$string);
+  $string    = str_replace(array("í","ì","î","ï") ,"i",$string);
+  $string    = str_replace(array("Í","Ì","Î","Ï") ,"i",$string);
 
-      $string    = str_replace(array("í","ì","î","ï") ,"i",$string);
-      $string    = str_replace(array("Í","Ì","Î","Ï") ,"i",$string);
+  $string    = str_replace(array("ó","ò","ô","õ","ö","º") ,"o",$string);
+  $string    = str_replace(array("Ó","Ò","Ô","Õ","Ö") ,"o",$string);
 
-      $string    = str_replace(array("ó","ò","ô","õ","ö","º") ,"o",$string);
-      $string    = str_replace(array("Ó","Ò","Ô","Õ","Ö") ,"o",$string);
+  $string    = str_replace(array("ú","ù","û","ü") ,"u",$string);
+  $string    = str_replace(array("Ú","Ù","Û","Ü") ,"u",$string);
 
-      $string    = str_replace(array("ú","ù","û","ü") ,"u",$string);
-      $string    = str_replace(array("Ú","Ù","Û","Ü") ,"u",$string);
+  $string = str_replace( "Ç", "C", $string);
+  $string = str_replace( "ç", "c", $string);
 
-      $string = str_replace( "Ç", "C", $string);
-      $string = str_replace( "ç", "c", $string);
+  $string = str_replace( "´", "", $string );
+  $string = str_replace( "`", "", $string );
+  $string = str_replace( "~", "", $string );
+  $string = str_replace( "^", "", $string );
+  $string = str_replace( "¨", "", $string );
+  $string = str_replace( ",", "", $string );
+  $string = str_replace( " ", "-", $string );
 
-      $string = str_replace( "´", "", $string );
-      $string = str_replace( "`", "", $string );
-      $string = str_replace( "~", "", $string );
-      $string = str_replace( "^", "", $string );
-      $string = str_replace( "¨", "", $string );
-      $string = str_replace( ",", "", $string );
-      $string = str_replace( " ", "-", $string );
+  $string = strtolower(utf8_decode($string));
 
-      $string = strtolower(utf8_decode($string));
+  return $string;
+}
 
-      echo $string;
-
-   }
-
-   echo url_amigavel('ação-bola');
-?>
+echo url_amigavel('ação-bola');
