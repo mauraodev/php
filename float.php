3b14a6bf608ec0b95223441fe1a3e8dda8950487
Mauro Nunes <mauraoemau@hotmail.com>
2018-10-18 23:11:39 -0300

    Test float

---
+++ float.php
@@ -0,0 +1,9 @@
+<?php
+
+$tradingValue = "1.60";
+$discount = "60";
+$totalSum = (float)$tradingValue + (float)$discount;
+
+echo $tradingValue . "<br>";
+echo $discount . "<br>";
+echo $totalSum . "<br>";
